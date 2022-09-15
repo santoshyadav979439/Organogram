648c111f4c10ddcd2edccfa5d2234681f5e89b81
Santosh <santosh.y@fnp.com>
2022-09-16 00:47:05 +0530

    handled diffrent roles

--- src/componnets/organisms/Datagrid/index.js
+++ src/componnets/organisms/Datagrid/index.js
@@ -5,21 +5,28 @@ import Thead from '../../molecules/thead';
 import { rawData } from './data';
 import axios from '../../../axios';
 import RowList from '../RowList';
+import { useDispatch, useSelector } from 'react-redux';
+import { setRole } from '../../../slices/role/roleSlice';
 const DataGrid = () => {
+  const role = useSelector((state) => state.roleSlice.value);
+  const dispatch = useDispatch();
   const [empData, setEmpData] = useState([]);
   useEffect(() => {
-    axios.get('getHiearachy/admin').then((res) => {
+    console.log('role is::', role);
+    axios.get('getHiearachy/' + role).then((res) => {
       let data = res.data;
 
       console.log(data);
       const { detail } = data;
       setEmpData(detail);
     });
-  }, []);
+  }, [role]);
   const rowClickHandler = (id) => {};
   const radioButtonClickHandler = (param) => {
     console.log(`${param} is clicked`);
+    dispatch(setRole(param));
   };
+  setRole;
   return (
     <div className='dataGrid'>
       <table id='organogram' style={{ display: 'table' }}>
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import popupSlice from '../slices/popup/popupSlice';
 import modify from '../slices/Caption/modifySlice';
+import roleSlice from '../slices/role/roleSlice';
 export const store = configureStore({
   reducer: {
     popup: popupSlice,
     modify,
+    roleSlice
   },
 });
