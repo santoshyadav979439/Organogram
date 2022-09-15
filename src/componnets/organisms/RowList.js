037745032ff532ea586ca0dc43ab0d56a7a6ef1b
Santosh <santosh.y@fnp.com>
2022-09-15 23:58:08 +0530

    added apis

--- src/componnets/organisms/RowList.js
+++ src/componnets/organisms/RowList.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 import Row from '../atoms/Row';
-
+import axios from '../../axios';
 const RowList = (props) => {
   const [isRowClicked, setIsRowClicked] = useState(false);
   const [hierarchy, setHierarchy] = useState([]);
-  const rowClickHandler = () => {
+  const rowClickHandler = (id) => {
     setIsRowClicked(true);
-    const data = [<Row {...props} rowClickHandler={rowClickHandler} />];
-    const newData = data.concat(
-      props.hierarchy.map((el) => (
-        <Row {...el} rowClickHandler={rowClickHandler} />
-      ))
-    );
-    setHierarchy(newData);
+    axios.get('admin/' + id).then((res) => {
+      const childData = res.data.detail;
+      const data = [<Row {...props} rowClickHandler={rowClickHandler} />];
+      const newData = data.concat(
+        childData.map((el) => <Row {...el} rowClickHandler={rowClickHandler} />)
+      );
+      setHierarchy(newData);
+    });
   };
   return !isRowClicked ? (
     <Row {...props} rowClickHandler={rowClickHandler} />
