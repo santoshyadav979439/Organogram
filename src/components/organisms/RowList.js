628201657087603059cd92e677bd984254cacbe1
FNP <karan.c@fnp.com>
2022-09-16 12:20:32 +0530

    minor

--- src/components/organisms/RowList.js
+++ src/components/organisms/RowList.js
@@ -4,45 +4,18 @@ import axios from "../../axios";
 const RowList = (props) => {
   const [isRowClicked, setIsRowClicked] = useState(false);
   const [hierarchy, setHierarchy] = useState([]);
-<<<<<<< HEAD
-  const rowClickHandler = (id) => {
-    console.log("@@", id);
-    setIsRowClicked(true);
-    const currentEmpId = id.split("-");
-    axios
-      .get("getHiearachy/admin/" + currentEmpId[currentEmpId.length - 1])
-      .then((res) => {
-        const childData = res.data.detail[0].hierarchy;
-        console.log("child data", childData);
-        const data = [<Row {...props} rowClickHandler={rowClickHandler} />];
-        const newData = data.concat(
-          childData?.map((el) => (
-            <Row
-              key={el.id}
-              {...el}
-              id={`${el.id}`}
-              rowClickHandler={rowClickHandler}
-            />
-          ))
-        );
-        console.log(newData);
-        console.log("row clicked", isRowClicked);
-        setHierarchy(newData);
-      })
-      .catch((err) => console.log(err));
-=======
   const rowClickHandler = (id, isExpanded, hierarchy) => {
-    console.log('h', hierarchy)
+    console.log("h", hierarchy);
     if (!isExpanded) {
       setIsRowClicked(false);
       return;
     }
     setIsRowClicked(true);
-    const currentEmpId = id.split('-');
+    const currentEmpId = id.split("-");
     let data = [<Row {...props} rowClickHandler={rowClickHandler} />];
     if (hierarchy == null) {
       axios
-        .get('getHiearachy/admin/' + currentEmpId[currentEmpId.length - 1])
+        .get("getHiearachy/admin/" + currentEmpId[currentEmpId.length - 1])
         .then((res) => {
           const childData = res.data.detail[0].hierarchy;
           const newData = data.concat(
@@ -73,7 +46,6 @@ const RowList = (props) => {
       data = [...data, ...h];
       setHierarchy(data);
     }
->>>>>>> bbacd681eb9980f69845b5edd334dcb78e8d431f
   };
   return !isRowClicked ? (
     <Row {...props} id={props.id} rowClickHandler={rowClickHandler} />
